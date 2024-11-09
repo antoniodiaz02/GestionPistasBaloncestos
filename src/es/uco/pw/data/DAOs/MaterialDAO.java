4ef10c171bacc78fc4c639cfa9ffdb2bd452872d
Antonio Diaz Barbancho <mesidiez@gmail.com>
2024-11-09 09:17:27 +0100

    Added jar mysql connector

--- src/es/uco/pw/data/DAOs/MaterialDAO.java
+++ src/es/uco/pw/data/DAOs/MaterialDAO.java
@@ -1,5 +1,23 @@
 package es.uco.pw.data.DAOs;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.mysql.jdbc.ResultSet;
+
+import es.uco.pw.business.DTOs.MaterialDTO;
+import es.uco.pw.common.DBConnection;
+
 public class MaterialDAO {
 
 }
